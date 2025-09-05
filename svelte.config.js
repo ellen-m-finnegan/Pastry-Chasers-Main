a04b87bc1bdc9d883a00bf34056cfa236547cf52
ellen-m-finnegan <efinnegan12@gmail.com>
2025-09-05 10:11:21 +0100

    updating svelte.config.js to explicitly set the output directory

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import { vitePreprocess } from '@astrojs/svelte';
+import { vitePreprocess } from "@astrojs/svelte";
 
 export default {
-	preprocess: vitePreprocess(),
-}
+  preprocess: vitePreprocess(),
+  outDir: "dist", // Explicitly set the output directory
+};
